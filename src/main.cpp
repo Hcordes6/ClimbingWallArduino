7a2cc7b4c74dfc3ed99c0a3c5a2155d450d16d24
Chizzicle <ccmoore5@wisc.edu>
2025-05-14 15:51:10 -0500

    added arduino functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <Arduino.h>
 
 // put function declarations here:
+const int dataPin = 2;   // SER
+const int clockPin = 3;  // SRCLK
+const int latchPin = 4;  // RCLK
+
+const int numRows = 12;
+const int numCols = 8;
+
+// 2D array representing LED states (1 = ON, 0 = OFF)
+byte ledState[12][8] = {
+  {1,0,0,0,0,0,0,0},
+  {0,1,0,0,0,0,0,0},
+  {0,0,1,0,0,0,0,0},
+  {0,0,0,1,0,0,0,0},
+  {0,0,0,0,0,0,0,0},
+  {0,0,0,1,1,1,1,0},
+  {0,0,0,0,0,0,1,0},
+  {0,0,0,0,0,0,0,1},
+  {0,0,0,0,1,0,0,0},
+  {0,0,0,1,0,0,0,0},
+  {0,0,0,0,1,0,0,0},
+  {0,0,0,0,1,0,0,0}
+};
 
 
 void setup() {
@@ -25,10 +47,46 @@ void setup() {
     }
     Serial.println("]");
   }
+
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  for (int row = 0; row < numRows; row++) {
+    // Create byte arrays for shift registers
+    byte rowData[2] = {0};   // For rows (only one bit HIGH at a time)
+    byte colData[1] = {0};   // For columns (1 = OFF, 0 = ON for active LEDs)
+
+    // Set the active row
+    if (row < 6) {
+      rowData[1] = (1 << row);
+    } else {
+      rowData[0] = (1 << (row - 6));
+    }
+
+    // Set columns based on ledState[row][col]
+    for (int col = 0; col < numCols; col++) {
+      if (ledState[row][col] == 1) {
+        if (col < 8) {
+          colData[0] &= ~(1 << col);  // LED ON = column LOW
+        } 
+      } else {
+        if (col < 8) {
+          colData[0] |= (1 << col);  // LED OFF = column HIGH
+        } 
+      }
+    }
+
+    // Shift out: first columns, then rows
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, colData[0]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowData[0]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowData[1]);
+    digitalWrite(latchPin, HIGH);
+
+    delayMicroseconds(1000); // Small delay
+  }
 }
 
